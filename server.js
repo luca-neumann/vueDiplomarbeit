4c93b5c8fa70198555bb10507ccf4270a135a0b2
lucaneumann <luca.ln.neumann@gmail.com>
2024-07-01 14:07:58 +0200

    Revert "serverabruf geht jetzt"
    This reverts commit 8ddd034409e6ba1527d9fc6a896809d891f0866a.

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-import express from 'express';
-import mysql from 'mysql';
-import cors from 'cors';
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
-const corsOptions = {
-  origin: '*', // Erlaubt Anfragen von allen Quellen. Für mehr Sicherheit, spezifische URL angeben.
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
@@ -38,7 +33,8 @@ app.get('/api/data', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000; // Ändern Sie den Port hier
+
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
