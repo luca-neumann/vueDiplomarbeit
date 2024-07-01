f44e2aa13a2cdd2c9f8d448658ae37511a76b9f1
lucaneumann <luca.ln.neumann@gmail.com>
2024-07-01 14:08:21 +0200

    Revert "test serverabfrage"
    This reverts commit 0023d07010c10976c016100d0203f98f4234efa4.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,47 @@
-<template>
-  <div id="app">
-    <DataTable />
-  </div>
-</template>
-
-<script>
-import DataTable from './components/DataTable.vue';
-
-export default {
-  name: 'App',
-  components: {
-    DataTable
-  }
-};
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
